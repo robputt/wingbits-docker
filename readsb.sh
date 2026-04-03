e60501e5d9cfbda84448e169d6fd2e9e74329edb
Robert.Putt <robert.putt@rackspace.co.uk>
2026-04-03 20:27:11 +0100

    Update to wingbits v1.1

--- readsb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source /etc/default/readsb
-/usr/bin/readsb $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS --write-json /run/readsb --quiet
--- wingbits.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /etc/wingbits; /usr/local/bin/wingbits feeder start
